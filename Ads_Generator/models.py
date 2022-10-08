f544daf77fa1495dc5f82fdb2bc993eefaec2cc5
kasiabauer <hellogithub00!>
2022-10-08 13:34:19 +0200

    Added models.py and 5 models. Added app name in settigs Migrated

---
+++ Ads_Generator/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+
+
+class Campaign(models.Model):
+    campaign_name = models.CharField(max_length=128)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.campaign_name}'
+
+
+class AdGroup(models.Model):
+    adgroup_name = models.CharField(max_length=128)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.adgroup_name}'
+
+
+class Keyword(models.Model):
+    keyword = models.CharField(max_length=64)
+    adgroup = models.ForeignKey(AdGroup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.keyword}'
+
+
+class AdText(models.Model):
+    adtext_headline_1 = models.CharField(max_length=64)
+    adtext_headline_2 = models.CharField(max_length=64)
+    adtext_description_1 = models.CharField(max_length=128)
+    adtext_description_2 = models.CharField(max_length=128)
+    adgroup = models.ForeignKey(AdGroup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.pk, self.adtext_headline_1}'
+
+
+class AdTextTemplate(models.Model):
+    adtext_template_headline_1 = models.CharField(max_length=64)
+    adtext_template_headline_2 = models.CharField(max_length=64)
+    adtext_template_description_1 = models.CharField(max_length=128)
+    adtext_template_description_2 = models.CharField(max_length=128)
+    campaign = models.ManyToManyField(Campaign, blank=True)
+
+    def __str__(self):
+        return f'{self.pk, self.adtext_template_headline_1}'
+
