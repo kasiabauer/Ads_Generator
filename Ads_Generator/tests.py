6e24d04d4362946d7d8035a7a55ea8650ca737e5
kasiabauer <hellogithub00!>
2022-10-22 11:38:05 +0200

    Added 2 tests for Generate Ad View

--- Ads_Generator/tests.py
+++ Ads_Generator/tests.py
@@ -544,3 +544,23 @@ def test_032_delete_adtext_template_get_without_login(client, adtext_templates):
     url = reverse('delete_template', args=(adtext_template.id, ))
     response = client.get(url)
     assert response.status_code == 200
+
+
+# 1st test for generate adtext view
+@pytest.mark.django_db
+def test_045_generate_ad_text_view_get_without_login(client, adgroups):
+    adgroup = adgroups[0]
+    url = reverse('generate_adtext', args=(adgroup.id, ))
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+# 2nd test for delete adtext template view
+@pytest.mark.django_db
+def test_046_generate_ad_text_view_get_logged_user(client, adgroups, users):
+    adgroup = adgroups[0]
+    user = users[0]
+    client.force_login(user)
+    url = reverse('generate_adtext', args=(adgroup.id, ))
+    response = client.get(url)
+    assert response.status_code == 200
